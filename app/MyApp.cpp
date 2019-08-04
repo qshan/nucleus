bbc2aa754c0c8e95a968396547b8beea30442391
Sean Whiteley <28666447+seghcder@users.noreply.github.com>
2019-08-04 15:37:02 +0800

    Per https://github.com/axomem/nucleus-alpha/issues/16, WIP example showing how to add new classes and vectors of class elements.

--- app/MyApp.cpp
+++ app/MyApp.cpp
@@ -15,6 +15,7 @@ using nlohmann::json;
 MyApp::MyApp()
     : p_message{make_persistent<experimental::string>("Hello World")}
     , p_update_count{0}
+    , p_customers{make_persistent<Customers>()}
 {
     Logging::log()->debug("MyApp Persistent Constructor called");
 }
@@ -33,7 +34,7 @@ MyApp::Initialize()
 {
     // child objects->Initialize any child objects here;
     Logging::log()->trace("MyApp is initializing");
-
+    p_customers->Initialize();
 }
 
 void
@@ -83,6 +84,7 @@ MyApp::Start(){
     // return the router to the RestServer
     RestServerRouter::getRestServerRouter().setRouter(std::move(router));
 
+    p_customers->Start();
     // App::init(this); RUNTIME App instance should be called here, if needed
 
 }
--- app/MyApp.hpp
+++ app/MyApp.hpp
@@ -4,6 +4,7 @@
 #ifndef MYAPP_H
 #define MYAPP_H
 #include "Nucleus.hpp"
+#include "Customers.hpp"
 
 using namespace pmem::obj;
 using namespace pmem::obj::experimental;
@@ -12,7 +13,7 @@ class MyApp {
 
 public:
     MyApp();            // this at pool creation or app reset. It does not run on each application start
-    ~MyApp();           // this happens when the App is being deleted from the pool. It is not called on app close.
+    ~MyApp();           // this happens when the class instance is being deleted from the pool. It is not called on app close.
 
     void Initialize();  // this happens at object creation, typically to init downstream objects that rely on this obj
     void Start();       // this happens each time the applications runs
@@ -22,6 +23,7 @@ private:
     // These are the persistent memory objects for this application.
     persistent_ptr<string> p_message;
     p<int> p_update_count;
+    persistent_ptr<Customers> p_customers;
 
 };
 
