fd00a4616da439c3883d92f3e60a62f1c98a5a5f
SeanW <28666447+seghcder@users.noreply.github.com>
2021-03-22 05:02:12 +0800

    Trying SonarQube integration. Adding windows install script but not working due to exception

---
+++ util/sonar-run.sh.py
@@ -0,0 +1,4 @@
+# Running in powershell but crashing due to SonarQUbe issue
+#  & $env:SONAR_SCANNER_HOME/bin/sonar-scanner.bat -D"sonar.organization=axomem" -D"sonar.projectKey=axomem_thingbase"
+#  -D"sonar.cfamily.build-wrapper-output=build/bw-output" -D"sonar.branch.name=update/202012v2"
+#  -D"sonar.host.url=https://sonarcloud.io"
